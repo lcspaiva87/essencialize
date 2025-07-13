45b22bfb70be3155fa966cb40757d9ce7be46cfe
Lucas Paiva <lcspaiva87@gmail.com>
2025-07-13 20:52:27 -0300

    feat: adds dialog component to recipes page and implements form for new recipe.

--- src/app/(dashboard)/receitas/page.tsx
+++ src/app/(dashboard)/receitas/page.tsx
@@ -15,12 +15,15 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { Edit, Eye, FilterIcon, MoreHorizontal, PlusIcon, SearchIcon, Trash2, X } from "lucide-react";
-
+import { Edit, Eye, FilterIcon, MoreHorizontal, PlusIcon, SearchIcon, Trash2, Wallet, X } from "lucide-react";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 export default function Receitas() {
-
-
-
   const Earnings = [
     {
       data: "2024-01-15",
@@ -46,7 +49,102 @@ export default function Receitas() {
         buttonText="Nova Receita"
         buttonIcon={<PlusIcon className="w-4 h-4" />}
       />
+      <Dialog
+        open={true}
+      >
+        <DialogTrigger>Open</DialogTrigger>
+        <DialogContent className="p-0">
+          <DialogHeader className="bg-primary p-4 rounded-t-lg flex  justify-between">
+            <DialogTitle className="text-white flex  gap-2">
+              <PlusIcon className="w-4 h-4" />
+              Nova Receita
+            </DialogTitle>
 
+          </DialogHeader>
+          <form className="flex flex-col gap-4 p-4">
+            <fieldset className="flex gap-2 flex-col">
+              <label htmlFor="descricao" className="text-sm font-medium text-gray-500">
+                Descrição
+              </label>
+              <Input
+                type="text"
+                id="descricao"
+                name="descricao"
+                aria-describedby="descricao-desc"
+              />
+              <span id="descricao-desc" className="sr-only">
+                Digite a descrição da receita
+              </span>
+            </fieldset>
+
+            <fieldset className="flex gap-2 flex-col">
+              <label htmlFor="valor" className="text-sm font-medium text-gray-500">
+                Valor
+              </label>
+              <div className="relative flex items-center w-full ">
+                <div className="absolute left-3 text-gray-400">
+                  <Wallet className="w-4 h-4" />
+                </div>
+                <div className="absolute left-10 inset-y-0 w-px bg-gray-300"></div>
+                <Input
+                  type="text"
+                  id="valor"
+                  name="valor"
+                  aria-describedby="valor-desc"
+                  placeholder="R$ 0,00"
+                  className="flex-grow pl-14 pr-4 py-2 w-full   focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all duration-200 ease-in-out "
+                />
+              </div>
+              <span id="valor-desc" className="sr-only">
+                Digite o valor da receita
+              </span>
+            </fieldset>
+
+            <fieldset className="flex gap-2 flex-col">
+              <label htmlFor="categoria" className="text-sm font-medium text-gray-500">
+                Categoria
+              </label>
+              <Select name="categoria">
+                <SelectTrigger id="categoria" aria-describedby="categoria-desc">
+                  <SelectValue placeholder="Selecione uma categoria" />
+                </SelectTrigger>
+              </Select>
+              <span id="categoria-desc" className="sr-only">
+                Digite a categoria da receita
+              </span>
+            </fieldset>
+
+
+            <fieldset className="flex gap-2 flex-col">
+              <label htmlFor="data" className="text-sm font-medium text-gray-500">
+                Data
+              </label>
+              <Input
+                type="date"
+                id="data"
+                name="data"
+                aria-describedby="data-desc"
+              />
+              <span id="data-desc" className="sr-only">
+                Digite a data da receita
+              </span>
+            </fieldset>
+
+            <fieldset className="flex gap-2 items-center">
+              <Input type="checkbox" id="recorrente" name="recorrente" className="w-4 h-4" />
+              <label htmlFor="recorrente" className="text-sm font-medium text-gray-500">
+                Recorrente
+              </label>
+            </fieldset>
+
+            <Button className="w-full text-white">
+              <PlusIcon className="w-4 h-4" />
+              Adicionar
+
+            </Button>
+          </form>
+        </DialogContent>
+      </Dialog>
       <section
         className="flex flex-col gap-6 bg-white rounded-xl shadow-sm border border-gray-100 p-6"
         aria-label="Filtros de receitas"
@@ -182,6 +280,6 @@ export default function Receitas() {
           </Table>
         </div>
       </section>
-    </main>
+    </main >
   )
 }
