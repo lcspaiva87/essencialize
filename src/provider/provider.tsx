7ee84003a38284556bf4ae2d5acd50fcd9c8588e
Lucas Paiva <lcspaiva87@gmail.com>
2025-07-22 22:45:58 -0300

    feat: Adds Domain Setup to Images and Layout Restructure with new Sidebar and Navigation Components

--- src/provider/provider.tsx
+++ src/provider/provider.tsx
@@ -1,11 +1,5 @@
-import { AppSidebar } from '@/components/layout/AppSidebar';
 import { SidebarProvider } from '@/components/ui/sidebar';
 
 export default function Provider({ children }: { children: React.ReactNode }) {
-  return (
-    <SidebarProvider>
-      <AppSidebar />
-      {children}
-    </SidebarProvider>
-  );
+  return <SidebarProvider>{children}</SidebarProvider>;
 }
